a3cff3abf8046228e81ec3e3b7afac54c488a407
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-05-06 22:34:00 +0300

    completed assignment

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import GifList from './GifList'
+import GifSearch from './GifSearch'
+
+function GifListContainer() {
+
+    const [dataFetch, setDataFetch] = useState()
+    const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${search === ""? "dolphin":search}&api_key=saanLrcouTbMFfTg26rXD2MrLPGObE7A&rating=g`)
+        .then(response => response.json())
+        .then((dataFetch) => setDataFetch(dataFetch.data.slice(0, 3)))
+    }, [search])
+    console.log(dataFetch)
+
+
+    function submitHandler (e) {
+      e.preventDefault()
+    }
+
+
+  return (
+    <div>
+      <GifSearch setSearch={setSearch} handler = {submitHandler}/>
+      <GifList dataFetch = {dataFetch}/>
+    </div>
+  )
+}
+
+export default GifListContainer;
